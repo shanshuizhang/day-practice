ace3a6d1a2a1490e2e9156f6f828e9e082d3b043
张富国、 <fuguo.zhang@jyblife.com>
2019-11-13 19:41:16 +0800

    提交项目代码

---
+++ mybatis-plus-starter/src/test/java/com/zss/mybatis/starter/MybatisPlusStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zss.mybatis.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
